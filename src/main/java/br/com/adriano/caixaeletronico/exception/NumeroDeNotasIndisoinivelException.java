4a26bd7c0f5a9555962059e82f5259bf5d60af15
adriano <adrianogaugusto@gmail.com>
2020-08-15 18:03:53 -0300

    melhorias

---
+++ src/main/java/br/com/adriano/caixaeletronico/exception/NumeroDeNotasIndisoinivelException.java
@@ -0,0 +1,8 @@
+package br.com.adriano.caixaeletronico.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.OK)
+public class NumeroDeNotasInvalidoException extends Exception {
+}
